28d55541852c88d662adb7225bbeebad5b8e6389
maksi <maksim.rylkin@mail.ru>
2025-05-06 15:08:20 +0300

    lab11+

--- src/main/java/ru/hpclab/hl/module1/kafkamodule/configuration/KafkaDispatcherConfig.java
+++ src/main/java/ru/hpclab/hl/module1/kafkamodule/configuration/KafkaDispatcherConfig.java
@@ -3,9 +3,9 @@ package ru.hpclab.hl.module1.kafkamodule.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.hpclab.hl.module1.apimodule.service.ArticleService;
-import ru.hpclab.hl.module1.apimodule.service.DownloadService;
-import ru.hpclab.hl.module1.apimodule.service.UserService;
+import ru.hpclab.hl.module1.service.LikesService;
+import ru.hpclab.hl.module1.service.PostService;
+import ru.hpclab.hl.module1.service.UserService;
 import ru.hpclab.hl.module1.kafkamodule.dispatch.KafkaMessageDispatcher;
 
 @Configuration
@@ -13,8 +13,8 @@ public class KafkaDispatcherConfig {
     @Bean
     public KafkaMessageDispatcher kafkaMessageDispatcher(
             UserService userService,
-            ArticleService articleService,
-            DownloadService downloadService,
+            PostService articleService,
+            LikesService downloadService,
             ObjectMapper objectMapper
     ) {
         return new KafkaMessageDispatcher(
